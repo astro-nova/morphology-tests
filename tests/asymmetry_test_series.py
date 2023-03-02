fix snr calculation

tests/asymmetry_test_series.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 import pickle
 import numpy as np
@@ -8,12 +9,13 @@ from joblib import Parallel, delayed
 import multiprocessing
 
 from astropy import units as u
-from photutils import EllipticalAperture, CircularAnnulus
+from photutils.aperture import EllipticalAperture, CircularAnnulus
 import galsim
 
 # from asymmetry import get_asymmetry
-from ..galaxy_generator import gen_image, gen_galaxy, petrosian_sersic, create_clumps, add_source_to_image, sky_noise, petrosian_sersic
-from ..asymmetry import get_asymmetry, get_residual
+sys.path.append('../')
+from galaxy_generator import gen_image, gen_galaxy, petrosian_sersic, create_clumps, add_source_to_image, sky_noise, petrosian_sersic
+from asymmetry import get_asymmetry, get_residual
 
 num_cores = multiprocessing.cpu_count()
 
@@ -24,7 +26,7 @@ plt.rcParams['axes.ymargin'] = .05  # y margin.  See `axes.Axes.margins`
 
 ##### Telescope parameters
 filt = 'r'
-bandpass_file = "passband_sdss_" + filt
+bandpass_file = "../passband_sdss_" + filt
 bandpass = galsim.Bandpass(bandpass_file, wave_type = u.angstrom)
 ## gain, exptime and diameter of telescope
 telescope_params = {'g':4.8, 't_exp':53.91, 'D':2.5}
@@ -65,7 +67,7 @@ def single_galaxy_run(filepath, mag, r_eff, sersic_n, q, n_clumps, sky_mag, psf_
     # convolve sources with psf and add to image
     image_psf = add_source_to_image(field, galaxy, clumps, all_xi, all_yi, psf_fwhm)
     # add sky and poisson noise
-    image_noisy = sky_noise(image_psf, sky_mag, pxscale, telescope_params, transmission_params, bandpass, rms_noise=True)
+    image_noisy, sky_flux = sky_noise(image_psf, sky_mag, pxscale, telescope_params, transmission_params, bandpass, rms_noise=True)
 
     image_perfect = image_perfect.array
     image_noisy = image_noisy.array
@@ -82,8 +84,7 @@ def single_galaxy_run(filepath, mag, r_eff, sersic_n, q, n_clumps, sky_mag, psf_
     ##### Calculate SNR
     ap_real = EllipticalAperture(x0_real, 2*r_pet, 2*r_pet)
     ap_sky = CircularAnnulus(x0_real, 2*1.5*r_pet, 2*2*r_pet)
-    sky_var = ap_sky.do_photometry(image_noisy**2)[0][0]
-    var = sky_var + image_perfect
+    var = sky_flux + image_perfect
     snr = image_perfect / np.sqrt(var)
     snr_px = ap_real.do_photometry(snr)[0][0] / ap_real.do_photometry(np.ones_like(snr))[0][0]
 
@@ -92,7 +93,7 @@ def single_galaxy_run(filepath, mag, r_eff, sersic_n, q, n_clumps, sky_mag, psf_
     aps = [x0_real, x0_cas, x0_sq]
     imgs = [image_perfect, image_noisy]
     inputs = dict(
-        mag=mag, r_eff=r_eff, sersic_n=sersic_n, q=q, n_clumps=n_clumps, 
+        mag=mag, r_eff=r_eff, r_pet=r_pet, sersic_n=sersic_n, q=q, n_clumps=n_clumps, 
         sky_mag=sky_mag, psf=psf_fwhm, snr=snr_px
     )
     output = {
@@ -110,23 +111,12 @@ def single_galaxy_run(filepath, mag, r_eff, sersic_n, q, n_clumps, sky_mag, psf_
 if __name__ == '__main__':
 
     ###### Parallelize over different galaxies. For each, do a PSF and SNR series.
-    
+
     # Parse input arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("N", help="number of galaxies to generate")
     parser.add_argument("path", help="folder to store images and asymmetries in")
     args = parser.parse_args()
-
-    # Telescope parameters
-    ## Transmission curve based on sdss r-band total throughput for airmass=1.3 extended source
-    filt = 'r'
-    bandpass_file = "passband_sdss_" + filt
-    bandpass = galsim.Bandpass(bandpass_file, wave_type = u.angstrom)
-    ## gain, exptime and diameter of telescope
-    telescope_params = {'g':4.8, 't_exp':53.91, 'D':2.5}
-    ## effective wavelength and width of filter
-    transmission_params = {'eff_wav':616.5, 'del_wav':137}
-
 
     ## Range of values to try
     lims = {
@@ -138,14 +128,14 @@ if __name__ == '__main__':
     }
 
     # Generate parameters for n galaxies
-    N = args.N
+    N = int(args.N)
     mags = stats.uniform.rvs(loc=lims['mag'][0], scale=lims['mag'][1] - lims['mag'][0], size=N)
     ns = stats.uniform.rvs(loc=lims['sersic_n'][0], scale=lims['sersic_n'][1] - lims['sersic_n'][0], size=N)
     sky_mags = stats.uniform.rvs(loc=lims['sky_mag'][0], scale=lims['sky_mag'][1] - lims['sky_mag'][0], size=N)
     n_clumps = np.random.randint(low=lims['n_clumps'][0], high=lims['n_clumps'][1], size=N)
     psfs = stats.uniform.rvs(loc=lims['psf_fwhm'][0], scale=lims['psf_fwhm'][1] - lims['psf_fwhm'][0], size=N)
     qs = stats.uniform.rvs(loc=0, scale=1, size=N)
-    rs = -1.9*mags + 35 + stats.normal.rvs(loc=0, scale=1.5, size=N)
+    rs = -1.9*mags + 35 + stats.norm.rvs(loc=0, scale=1.5, size=N)
     rs[rs <= 1] = 1
 
     ### Run the execution in parallel